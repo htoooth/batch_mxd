6b84418c53876d57f3c6f650e657fc8733e2f441
Tao Huang <ht.anglenx@gmail.com>
2014-11-04 17:03:10 +0800

    add save arcgis 9.3 mxd

--- savemxdwithversion.py
+++ savemxdwithversion.py
@@ -1,28 +1,16 @@
-
 import os
 import arcpy
 
-input_path  = arcpy.GetParameterAsText(0)
-resolution  = arcpy.GetParameterAsText(1)
-isexportTree   = arcpy.GetParameterAsText(2)
-output_path = arcpy.GetParameterAsText(3)
 
-def export_mxd(inputfile,output,res):
-    mxd = arcpy.mapping.MapDocument(inputfile)
-    arcpy.mapping.ExportToJPEG(mxd,output,resolution=res)
+input_path  = arcpy.GetParameterAsText(0)
+output_path = arcpy.GetParameterAsText(1)
 
 def ignore_file(path,names):
     return [name for name in names if os.path.isfile(os.path.join(path,name)) and os.path.splitext(name)[1].lower() != ".mxd"]
 
-def export_into_one(src,dst,handle = None,ignore = None):
-    names = []
-    for (path,dirs,files) in os.walk(src):
-        names.extend([ (path,i) for i in files if os.path.splitext(i)[1].lower() == ".mxd"])
-
-    for (path,name) in names:
-        i = os.path.join(path,name)
-        o = os.path.join(dst,os.path.splitext(name)[0] + ".jpg")
-        handle(i,o)
+def save_mxd(inputfile,output,ver="9.3"):
+    mxd = arcpy.mapping.MapDocument(inputfile)
+    mxd.saveACopy(output,ver)
 
 def export_mxd_tree(src,dst,handle = None,ignore = None):
     names = os.listdir(src)
@@ -41,7 +29,7 @@ def export_mxd_tree(src,dst,handle = None,ignore = None):
         os.makedirs(dst)
 
     dst = dst.decode('gb2312')
-    
+
     for name in names:
         if name in ignored_names:
             continue
@@ -52,10 +40,5 @@ def export_mxd_tree(src,dst,handle = None,ignore = None):
         else:
             handle(srcname,dstname)
 
-def handle_mxd(res):
-    return lambda i,o:export_mxd(i,o,res)
 
-if str(isexportTree) == 'true':
-    export_mxd_tree(input_path,output_path,handle_mxd(resolution),ignore_file)
-else:
-    export_into_one(input_path,output_path,handle_mxd(resolution))
+export_mxd_tree(input_path,output_path,save_mxd,ignore_file)
